60e69b4b1e13f4e36d29d3ac5b22b6abbf1fbce6
MeherShroff2 <m.r.shroff@student.tue.nl>
2025-05-05 19:24:24 +0200

    Added comments to files of dependabot plugin

--- my-backstage-app/plugins/tech-insights-backend-module-dependabot/src/index.ts
+++ my-backstage-app/plugins/tech-insights-backend-module-dependabot/src/index.ts
@@ -1,1 +1,2 @@
+//this file is an entry point to expose what is in module.ts
 export { default } from './module';
--- my-backstage-app/plugins/traffic-light-backend/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { LoggerService } from '@backstage/backend-plugin-api';
-import { Config } from '@backstage/config';
-import { Router } from 'express';
-
-export interface PluginEnvironment {
-  logger: LoggerService;
-  config: Config;
-  router: Router;
-}
